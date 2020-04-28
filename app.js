8a02f7860af32220b8868340d983d118e2697d99
ArrowX17 <iamsamix17@gmail.com>
2020-04-28 17:32:26 +0600

    Console log the post title

--- app.js
+++ app.js
@@ -34,8 +34,13 @@ app.get("/contact", function(req, res) {
   });
 });
 
-
-
+app.get("/compose", function(req, res) {
+  res.render("compose");
+});
+app.post("/compose", function(req, res) {
+  var message = req.body.postTitle;
+  console.log("message", message);
+});
 
 
 
